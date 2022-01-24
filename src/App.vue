5d9ed89997404852a27214e11f1de944fe000584
Hwang-Saingjin <aosdb5640@naver.com>
2022-01-24 16:30:42 +0900

    methos function

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
-    <h1>{{message}}</h1>
-    <HelloWorld/>
+  <h1>{{ count }}</h1>
+  <button @click="increase">Plus</button>
+  <button @click="decrease">Minus</button>
 </template>
 
 <script>
-import HelloWorld from '~/components/HelloWorld'
-
-    export default {
-        components: {
-            HelloWorld
-        },
-        data() {
-            return {
-                message: 'Hello Vue!'
-            }
-        }
+export default {
+  data() {
+    return {
+      count : 0
     }
+  },
+  methods: {
+      increase() {
+          this.count +=1
+      },
+      decrease(){
+          this.count-=1
+      }
+  }
+}
+
 </script>
 
-<style scoped>
-
+<style>
+    h1{
+        font-size: 50px;
+        color: royalblue;
+    }
 </style>
