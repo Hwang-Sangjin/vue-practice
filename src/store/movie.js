52b8f2d0feaaab88cd64621f97a2388a864bf5ed
jin_itemscout <aosdb5640@naver.com>
2022-01-27 21:22:45 +0900

    vuex store

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,3 +1,4 @@
+import axios from "axios"
 export default {
     // module
     namespaced :true,
@@ -8,24 +9,29 @@ export default {
         }
     },
     //computed
-    getters: {
-        movieIds(state) {
-            return state.movies.map(m => m.imdbID)
-        }
-    },
+    getters: {},
     // methods
     // 변이, data 변경
     mutations: {
+        updateState(state, payload) {
+            Object.keys(payload).forEach(key => {
+                state[key] = payload[key]
+            })
+        },
         resetMovies(state){
             state.movies = []
         }
     },
     // 비동기
     actions: {
-        searchMovies(context){
-            context.state
-            context.getters
-            context.commit
+        async searchMovies(context, payload){
+            const {title, type, number, year} = payload
+            const OMDB_API_KEY = '7035c60c'
+            const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
+            const {Search, totalResults} = res.data
+            context.commit('updateState',{
+                movies: Search
+            })
         }
     }
 }
