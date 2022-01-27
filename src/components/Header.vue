31865bfd39a66d4ec94d50d45e3b68b17c4359c9
jin_itemscout <jin@itemscout.io>
2022-01-27 12:38:58 +0900

    navigation header

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header class="nav">
+        <div
+            v-for="nav in navigation"
+            :key="nav.name"
+            class="nav-item">
+            
+            <RouterLink
+                :to="nav.href"
+                class="nav-link">
+                {{nav.name}}
+            </RouterLink>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            navigation: [
+                {
+                    name: 'Search',
+                    href: '/'
+                },
+                {
+                    name: 'Movie',
+                    href: '/movie'
+                },
+                {
+                    name: 'About',
+                    href: '/about'
+                },
+            ]
+        }
+    }
+}
+</script>
