8103998743858171a827ecd01d4f3c21e277df0b
jin_itemscout <aosdb5640@naver.com>
2022-02-03 16:07:40 +0900

    poster exception

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from "vue"
 import App from "./App"
 import router from './routes/index.js'
 import Store from './store/index.js'
+import loadImage from './plugins/loadImage'
 
 
 createApp(App)
     .use(router)
     .use(Store)
+    .use(loadImage)
     .mount("#app")
