30f02aeb2ed2fcdc3cdc5750d4d87c2c5879c304
jin_itemscout <aosdb5640@naver.com>
2022-02-04 14:26:38 +0900

    scroll 초기화

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 import _uniqBy from "lodash/uniqBy"
 
+const _defaultMessage = "Search for the movie title!"
+
 export default {
     // module
     namespaced :true,
@@ -8,7 +10,7 @@ export default {
     state: () => {
         return {
             movies: [],
-            message: 'Search for the movie title!',
+            message: _defaultMessage,
             loading: false,
             theMovie: {}
         }
@@ -24,7 +26,9 @@ export default {
             })
         },
         resetMovies(state){
-            state.movies = []
+            state.movies = [],
+            state.message = _defaultMessage,
+            state.loading = false
         }
     },
     // 비동기
